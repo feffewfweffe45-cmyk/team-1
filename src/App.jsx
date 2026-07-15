470f7179b0c31ac88280b673b49c034b46cc2b86
feffewfweffe45-cmyk <feffewfweffe45@gmail.com>
2026-07-15 09:38:38 +0500

    team 1 ozgarish kiritildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
-
+import Browse from "./components/browse/Browse";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Furniture from "./components/furniture/Furniture";
+import Room from "./components/room/Room";
+import Hero from "./components/hero/Hero";
+import Product from "./components/product/Product";
 function App() {
   return (
     <div className="app">
-      <div className="app-wr"></div>
+      <div className="app-wr">
+        <Browse />
+        <Navbar />
+        <Furniture />
+        <Room />
+        <Footer />
+        <Hero />
+        <Product />
+      </div>
     </div>
   );
 }
---
+++ src/components/room/Room.jsx
@@ -0,0 +1,6 @@
+function Room(){
+  return (
+    <div></div>
+  )
+}
+export default Room
